be23f5b40e8ab805e8a51e3b9f09e0717b616099
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-12-22 15:38:41 +0700

    wip

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -22,7 +22,7 @@ pub struct MerkleTree<V: Leafable> {
 }
 
 impl<V: Leafable> MerkleTree<V> {
-    pub fn new(mock_db: &mut MockDB<V>, height: usize) -> Self {
+    pub async fn new(mock_db: &MockDB<V>, height: usize) -> Self {
         // zero_hashes = reverse([H(zero_leaf), H(H(zero_leaf), H(zero_leaf)), ...])
         let mut zero_hashes = vec![];
         let mut h = V::empty_leaf().hash();
@@ -30,13 +30,15 @@ impl<V: Leafable> MerkleTree<V> {
         for _ in 0..height {
             let new_h = Hasher::<V>::two_to_one(h, h);
             zero_hashes.push(new_h);
-            mock_db.insert(
-                new_h,
-                Node {
-                    left: h.clone(),
-                    right: h.clone(),
-                },
-            );
+            mock_db
+                .insert(
+                    new_h,
+                    Node {
+                        left_hash: h.clone(),
+                        right_hash: h.clone(),
+                    },
+                )
+                .await;
             h = new_h;
         }
         zero_hashes.reverse();
@@ -75,14 +77,8 @@ impl<V: Leafable> MerkleTree<V> {
     }
 
     // index_bits is little endian
-    pub fn update_leaf(
-        &mut self,
-        mock_db: &mut MockDB<V>,
-        index_bits: Vec<bool>,
-        leaf_hash: HashOut<V>,
-    ) {
-        assert_eq!(index_bits.len(), self.height);
-        let mut path = index_bits;
+    pub async fn update_leaf(&mut self, mock_db: &MockDB<V>, index: u64, leaf_hash: HashOut<V>) {
+        let mut path = u64_le_bits(index, self.height());
         path.reverse(); // path is big endian
 
         let mut h = leaf_hash;
@@ -98,43 +94,29 @@ impl<V: Leafable> MerkleTree<V> {
             };
             self.node_hashes.insert(path.clone(), new_h.clone());
             let node = Node {
-                left: if b { sibling } else { h.clone() },
-                right: if b { h.clone() } else { sibling },
+                left_hash: if b { sibling } else { h.clone() },
+                right_hash: if b { h.clone() } else { sibling },
             };
-            mock_db.insert(new_h.clone(), node);
+            mock_db.insert(new_h.clone(), node).await;
             h = new_h;
         }
     }
 
-    pub fn prove(&self, index_bits: Vec<bool>) -> MerkleProof<V> {
-        assert_eq!(index_bits.len(), self.height);
-        let mut path = index_bits;
-        path.reverse(); // path is big endian
-
-        let mut siblings = vec![];
-        while !path.is_empty() {
-            siblings.push(self.get_sibling_hash(&path));
-            path.pop();
-        }
-        MerkleProof { siblings }
-    }
-
-    pub fn prove_with_given_root(
+    pub async fn prove_with_given_root(
         &self,
         mock_db: &MockDB<V>,
         root: HashOut<V>,
-        index_bits: Vec<bool>,
+        index: u64,
     ) -> MerkleProof<V> {
-        assert_eq!(index_bits.len(), self.height);
-        let mut path = index_bits;
+        let mut path = u64_le_bits(index, self.height());
         let mut siblings = vec![];
         let mut hash = root;
         while !path.is_empty() {
-            let node = mock_db.get(hash).expect("cannot find node");
+            let node = mock_db.get(hash).await.expect("cannot find node");
             let (child, sibling) = if path.pop().unwrap() {
-                (node.right, node.left)
+                (node.right_hash, node.left_hash)
             } else {
-                (node.left, node.right)
+                (node.left_hash, node.right_hash)
             };
             siblings.push(sibling);
             hash = child;
@@ -185,8 +167,9 @@ impl<V: Leafable> MerkleProof<V> {
         self.siblings.len()
     }
 
-    pub fn get_root(&self, leaf_data: &V, index_bits: Vec<bool>) -> HashOut<V> {
+    pub fn get_root(&self, leaf_data: &V, index: u64) -> HashOut<V> {
         let mut state = leaf_data.hash();
+        let index_bits = u64_le_bits(index, self.height());
         for (&bit, sibling) in index_bits.iter().zip(self.siblings.iter()) {
             state = if bit {
                 Hasher::<V>::two_to_one(*sibling, state)
@@ -197,21 +180,16 @@ impl<V: Leafable> MerkleProof<V> {
         state
     }
 
-    pub fn verify(
-        &self,
-        leaf_data: &V,
-        index_bits: Vec<bool>, // little endian
-        merkle_root: HashOut<V>,
-    ) -> anyhow::Result<()> {
+    pub fn verify(&self, leaf_data: &V, index: u64, merkle_root: HashOut<V>) -> anyhow::Result<()> {
         anyhow::ensure!(
-            self.get_root(leaf_data, index_bits) == merkle_root,
+            self.get_root(leaf_data, index) == merkle_root,
             "Merkle proof verification failed"
         );
         Ok(())
     }
 }
 
-pub fn usize_le_bits(num: usize, length: usize) -> Vec<bool> {
+pub fn u64_le_bits(num: u64, length: usize) -> Vec<bool> {
     let mut result = Vec::with_capacity(length);
     let mut n = num;
     for _ in 0..length {
@@ -225,35 +203,35 @@ pub fn usize_le_bits(num: usize, length: usize) -> Vec<bool> {
 mod test {
     use intmax2_zkp::utils::{leafable::Leafable, poseidon_hash_out::PoseidonHashOut};
 
-    use crate::{merkle_tree::usize_le_bits, mock_db::MockDB};
+    use crate::mock_db::MockDB;
 
     use super::MerkleTree;
 
     type Leaf = u32;
 
-    #[test]
-    fn test_prove_with_given_root() {
+    #[tokio::test]
+    async fn test_prove_with_given_root() {
         let height = 32;
 
-        let mut mock_db = MockDB::<Leaf>::new();
-        let mut merkle_tree = MerkleTree::new(&mut mock_db, height);
+        let mock_db = MockDB::<Leaf>::new();
+        let mut merkle_tree = MerkleTree::new(&mock_db, height).await;
 
         for i in 0..10 {
             let leaf = i as u32;
-            let index_bits = super::usize_le_bits(i, height);
-            merkle_tree.update_leaf(&mut mock_db, index_bits, leaf.hash());
+            merkle_tree.update_leaf(&mock_db, i, leaf.hash()).await;
         }
         let root1 = merkle_tree.get_root();
         for i in 10..20 {
             let leaf_hash = PoseidonHashOut::hash_inputs_u32(&[i as u32]);
-            let index_bits = usize_le_bits(i, height);
-            merkle_tree.update_leaf(&mut mock_db, index_bits, leaf_hash);
+            merkle_tree.update_leaf(&mock_db, i, leaf_hash).await;
         }
         let index = 6;
         let leaf = index as u32;
-        let index_bits = super::usize_le_bits(index, height);
-        let proof = merkle_tree.prove_with_given_root(&mock_db, root1, index_bits.clone());
-        let root1_expected = proof.get_root(&leaf, index_bits);
+        let proof = merkle_tree
+            .prove_with_given_root(&mock_db, root1, index)
+            .await;
+        proof.verify(&leaf, index, root1).unwrap();
+        let root1_expected = proof.get_root(&leaf, index);
         assert_eq!(root1, root1_expected);
     }
 }
--- src/mock_db.rs
+++ src/mock_db.rs
@@ -1,33 +1,37 @@
+use std::sync::Arc;
+
 use hashbrown::HashMap;
 use intmax2_zkp::utils::{leafable::Leafable, leafable_hasher::LeafableHasher};
+use tokio::sync::RwLock;
+
+type Hasher<V> = <V as Leafable>::LeafableHasher;
+type HashOut<V> = <Hasher<V> as LeafableHasher>::HashOut;
 
 #[derive(Clone, Debug)]
 pub struct Node<V: Leafable> {
-    pub left: <V::LeafableHasher as LeafableHasher>::HashOut,
-    pub right: <V::LeafableHasher as LeafableHasher>::HashOut,
+    pub left_hash: HashOut<V>,
+    pub right_hash: HashOut<V>,
 }
 
-pub trait NodeDB<V: Leafable> {
-    
-}
+pub trait NodeDB<V: Leafable> {}
 
 #[derive(Clone, Debug)]
 pub struct MockDB<V: Leafable> {
-    nodes: HashMap<<V::LeafableHasher as LeafableHasher>::HashOut, Node<V>>, // parents hash to node (2 child hashes)
+    nodes: Arc<RwLock<HashMap<HashOut<V>, Node<V>>>>, // parents hash to node (2 child hashes)
 }
 
 impl<V: Leafable> MockDB<V> {
     pub fn new() -> Self {
         MockDB {
-            nodes: HashMap::new(),
+            nodes: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
-    pub fn insert(&mut self, key: <V::LeafableHasher as LeafableHasher>::HashOut, node: Node<V>) {
-        self.nodes.insert(key, node);
+    pub async fn insert(&self, parent_hash: HashOut<V>, node: Node<V>) {
+        self.nodes.write().await.insert(parent_hash, node);
     }
 
-    pub fn get(&self, key: <V::LeafableHasher as LeafableHasher>::HashOut) -> Option<Node<V>> {
-        self.nodes.get(&key).cloned()
+    pub async fn get(&self, parent_hash: HashOut<V>) -> Option<Node<V>> {
+        self.nodes.read().await.get(&parent_hash).cloned()
     }
 }
