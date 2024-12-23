c758c88972dd576b6af30c85cc31b0300cf5ea72
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-12-23 18:34:30 +0700

    wip

--- migrations/20241215172736_initial.up.sql
+++ migrations/20241215172736_initial.up.sql
@@ -1,20 +1,24 @@
 CREATE TABLE IF NOT EXISTS hash_nodes (
+    tag int NOT NULL,
     parent_hash bytea PRIMARY KEY,
     left_hash  bytea NOT NULL,
     right_hash bytea NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS current_leaf_hashes (
+    tag int NOT NULL,
     position bigint PRIMARY KEY,
     leaf_hash bytea NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS current_leaves (
-    position bigint PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS leaves (
+    tag int NOT NULL,
+    leaf_hash bytea PRIMARY KEY,
     leaf bytea NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS root_history (
+    tag int NOT NULL,
     i int PRIMARY KEY,
     root bytea NOT NULL
 );
