a4bd97650adf8fe185441e5e2e305c34be3bd3d6
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-12-25 19:06:31 +0700

    wip

--- migrations/20241215172736_initial.down.sql
+++ migrations/20241215172736_initial.down.sql
@@ -1,6 +1,3 @@
 DROP TABLE IF EXISTS hash_nodes;
 DROP TABLE IF EXISTS leaves;
 DROP TABLE IF EXISTS leaves_len;
-
-DROP TABLE IF EXISTS indexed_merkle_leaves;
-DROP TABLE IF EXISTS root_history;
--- migrations/20241215172736_initial.up.sql
+++ migrations/20241215172736_initial.up.sql
@@ -22,21 +22,4 @@ CREATE TABLE IF NOT EXISTS leaves_len (
     PRIMARY KEY (timestamp_value, tag)
 );
 
-CREATE TABLE IF NOT EXISTS indexed_merkle_leaves (
-    timestamp_value bigint NOT NULL,
-    tag int NOT NULL,
-    position bigint NOT NULL,
-    leaf_hash bytea NOT NULL,
-    next_index_value bigint,
-    key_value NUMERIC(78),
-    next_key_value NUMERIC(78),
-    value_value bigint,
-    PRIMARY KEY (timestamp_value, tag, position)
-);
 
-CREATE TABLE IF NOT EXISTS root_history (
-    timestamp_value bigint NOT NULL,
-    tag int NOT NULL,
-    root_value bytea NOT NULL,
-    PRIMARY KEY (tag, timestamp_value)
-);
